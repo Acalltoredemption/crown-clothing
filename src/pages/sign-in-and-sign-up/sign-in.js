efe95fd3876f667ac7dc00ecc35fad22d1845f3a
Acalltoredemption <acalltoredemption@gmail.com>
2020-10-20 18:52:04 -0700

    Begun work on the signin page

---
+++ src/pages/sign-in-and-sign-up/sign-in.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './sign-in.scss';
+import SignIn from '../../components/sign-in/signin';
+
+
+const SignInAndSignUpPage = () => (
+
+    <div className='sign-in-and-sign-up'>
+        <SignIn />
+    </div>
+
+);
+
+export default SignInAndSignUpPage;
