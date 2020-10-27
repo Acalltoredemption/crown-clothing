1c40b1d848cbe0849959f47af1e7c5c97356166a
Acalltoredemption <acalltoredemption@gmail.com>
2020-10-27 11:29:27 -0700

    implemented firebase sign in/sign up for users in firestore database

--- src/pages/sign-in-and-sign-up/sign-in.js
+++ src/pages/sign-in-and-sign-up/sign-in.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './sign-in.scss';
 import SignIn from '../../components/sign-in/signin';
-
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignInAndSignUpPage = () => (
 
     <div className='sign-in-and-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
 
 );
